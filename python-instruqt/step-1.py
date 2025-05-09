a76d88f40d10d6449a290966ea4814c1ea1545ca
Eduard Knezovic <knezovic.eduard@gmail.com>
2025-05-09 19:41:09 +0200

    add the python instruqt files

---
+++ python-instruqt/step-1.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+from cassandra.cluster import Cluster
+from cassandra import ConsistencyLevel
+
+
+class App:
+    def __init__(self):
+        self.cluster = Cluster(contact_points=[
+            ("localhost", 9042),  # node1
+            ("localhost", 9043),  # node2
+            ("localhost", 9044)   # node3
+        ])
+        self.session = self.cluster.connect(keyspace="catalog")
+        self.session.default_consistency_level = ConsistencyLevel.QUORUM
+
+    def stop(self):
+        self.cluster.shutdown()
+
+if __name__ == "__main__":
+    app = App()
+    print(f"ScyllaDB session: {app.session}")
+    app.stop()
+
---
+++ python-instruqt/step-2.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+from cassandra.cluster import Cluster
+from cassandra import ConsistencyLevel
+
+
+class App:
+    def __init__(self):
+        self.cluster = Cluster(contact_points=[
+            ("localhost", 9042),  # node1
+            ("localhost", 9043),  # node2
+            ("localhost", 9044)   # node3
+        ])
+        self.session = self.cluster.connect(keyspace="catalog")
+        self.session.default_consistency_level = ConsistencyLevel.QUORUM
+
+    def show_mutant_data(self):
+        print("Data that we have in the catalog".center(50, "="))
+        result = self.session.execute(query="SELECT * FROM mutant_data")
+        for row in result:
+            print(row.first_name, row.last_name)
+        return result
+
+    def stop(self):
+        self.cluster.shutdown()
+
+if __name__ == "__main__":
+    app = App()
+    print(f"ScyllaDB session: {app.session}")
+    result = app.show_mutant_data()
+    print(result)
